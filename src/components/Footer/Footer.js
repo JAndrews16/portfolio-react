069e51972d39832edad8e757464a46909c43465e
Juliana Andrews <julianakandrews@gmail.com>
2020-11-10 19:41:39 -0800

    updated componenets

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,16 +2,19 @@ import React from "react";
 
 const footerStyle = {
     position: "fixed",
-    marginTop: "80%"
+    marginTop: "50%",
+    color: "beige",
+    fontSize: "10px",
+    backgroundColor: "rgb(58, 58, 58)"
 }
 
 const Footer = () => {
     return (
         <>
             <div className="attributions" style={footerStyle}>
-                Icons made by <a href="https://icon54.com/" title="Pixel perfect">Pixel perfect</a> and 
-                <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from 
-                <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+                Icons made by <a href="https://icon54.com/">Pixel perfect</a> and 
+                <a href="https://www.flaticon.com/authors/freepik">Freepik</a> from 
+                <a href="https://www.flaticon.com/">www.flaticon.com</a>
             </div>
         </>
     )
